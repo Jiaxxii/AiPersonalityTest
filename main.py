152e18b8078e910d3ed020e7d299d267b55ea9bb
西雨与雨 <101635603+Jiaxxii@users.noreply.github.com>
2025-01-30 17:18:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ class MyPlugin(BasePlugin):
             # 回复消息 "hello, <发送者id>!"
             ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
 
+            ctx.add_return("reply", ["hello, everyone!"])
+
             # 阻止该事件默认行为（向接口获取回复）
             ctx.prevent_default()
 
